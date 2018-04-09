4cdaf373abcec3fbbdbf0fee781deab175f0a98d
lehrmann <lukas.ehrmann@gmail.com>
2018-04-09 09:38:47 +0200

    Setup Script

--- setup_rpi.sh
+++ setup_rpi.sh
@@ -7,7 +7,9 @@ sudo apt upgrade -y
 # Install apt packages
 sudo apt install -y python3
 sudo apt install -y python3-pip
+sudo apt-get install pigpio python-pigpio python3-pigpio
 
 # Update pip3 and install the required python libraries
 sudo pip3 install -U setuptools
 sudo pip3 install -U pip
+sudo pip3 install tornado
